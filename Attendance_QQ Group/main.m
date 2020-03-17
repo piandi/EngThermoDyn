d9796909307fb0dad5c60710be460d828125fcaf
GGQ_sb1703 <gqguan@hotmail.com>
2020-03-17 23:57:34 +0800

    修正QQ群用户昵称不规范造成的用户信息识别问题

--- Attendance_QQ Group/main.m
+++ Attendance_QQ Group/main.m
@@ -16,8 +16,12 @@ i = 1;
 while i<(line_num)
     % 扫描第i行
     content = textscan(QQGroup_log(i),'%s');
-    content = content{:};    
-    if length(content) == 3 && length(content{1}) == 10 && length(content{2}) == 8
+    content = content{:};
+    % debug use only
+    if i == 2627
+        i
+    end
+    if (length(content{1}) == 10) && (content{1}(5) == content{1}(8))
         % 标识该行为header
         log_sn = log_sn+1;
         dates(log_sn) = textscan(content{1},'%D');
@@ -38,5 +42,96 @@ while i<(line_num)
     i = i+1;
 end
 contents(log_sn) = cellstr(QQGroup_log(i));
-dates = dates'; times = times'; users = users'; contents = contents'; QQNum = QQNum';
+% convert cell array of datetime to array of datetime
+dates = [dates{:}]'; times = [times{:}]'; 
+users = users'; contents = contents'; QQNum = QQNum';
 log_tab = table(dates,times,QQNum, users,contents);
+
+%% 提取指定日期的记录
+% 输入提取数据的日期
+setdate = datetime(2020,3,17);
+% 提取记录
+extract_log = log_tab(dates == setdate,:);
+
+%% 提取上课打卡记录
+% 上课码
+flag_start = '110';
+% 提取上课打码记录
+idx_start = strcmp(extract_log.contents, flag_start);
+user1 = extract_log.users(idx_start);
+user1 = [user1{:}]';
+QQNum = extract_log.QQNum(idx_start);
+Start = extract_log.times(idx_start);
+% 检查重复打卡
+[~,idx_start_unique] = unique(user1);
+user1 = user1(idx_start_unique);
+QQNum = QQNum(idx_start_unique);
+Start = Start(idx_start_unique);
+% 列出学生姓名和学号
+list1 = table(QQNum,user1,Start);
+
+%% 提取下课打卡记录
+% 下课码
+flag_end = '119';
+% 提取下课打码记录
+idx_end = strcmp(extract_log.contents, flag_end);
+user2 = extract_log.users(idx_end);
+user2 = [user2{:}]';
+QQNum = extract_log.QQNum(idx_end);
+End = extract_log.times(idx_end);
+% 检查重复打卡
+[~,idx_end_unique] = unique(user2);
+user2 = user2(idx_end_unique);
+QQNum = QQNum(idx_end_unique);
+End = End(idx_end_unique);
+% 列出学生姓名和学号
+list2 = table(QQNum,user2,End);
+
+%% 添加下课打卡记录
+nostart_idx = zeros(size(user2));
+Remarks = zeros(size(user1));
+EndTime = Start;
+for i = 1:height(list1)
+    get_idx = strcmp(user2, user1{i});
+    if all(get_idx == 0)
+        fprintf('Cannot find the finish flag for user: %s\n',list1.user1{i})
+        Remarks(i) = 1;
+    else
+        EndTime(i) = list2.End(get_idx);
+    end
+    nostart_idx = nostart_idx+get_idx;
+end
+End = EndTime;
+attendance_list = [list1,table(End,Remarks)]; 
+if sum(nostart_idx) < length(user2)
+    nostart_idx = ~logical(nostart_idx);
+    fprintf('Cannot find the start flag for user: %s\n',list2.user2{nostart_idx})
+    % 增列无开始记录的学生
+    QQNum = QQNum(nostart_idx);
+    user1 = user2(nostart_idx); 
+    Start = End(nostart_idx);
+    End = End(nostart_idx);
+    Remarks = ones(size(QQNum))*2;
+    attendance_list = [attendance_list;table(QQNum,user1,Start,End,Remarks)];
+end
+%% 输出打卡记录
+% 绘图
+for i = 1:height(attendance_list)
+    switch attendance_list.Remarks(i) 
+        case(0)
+            period = [attendance_list.Start(i);attendance_list.End(i)];
+            plot(period,ones(size(period))*i,'-b','LineWidth',5)
+        case(1)
+            plot(attendance_list.Start(i),i,'>r')
+        case(2)
+            plot(attendance_list.End(i),i,'<r')
+    end
+    hold on
+end
+hold off
+ylim([0,height(attendance_list)+1])
+datetick('x','HH:MM:SS')
+% set(gca,'YTick',1:height(ImportData),'YTickLabel',ImportData.Nickname)
+set(gca,'YTick',1:height(attendance_list),'YTickLabel',cellfun(@string,attendance_list.user1))
+xlabel(['时间',' | ',datestr(setdate)])
+ylabel('上课人员昵称')
