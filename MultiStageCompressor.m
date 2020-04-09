cf5c498410e8ccfed9053f5f478d70101df4d40d
GGQ_sb1703 <gqguan@hotmail.com>
2020-04-09 12:45:11 +0800

    [feature] 采用数值求解代替原符号运行以提高运行效率

--- MultiStageCompressor.m
+++ MultiStageCompressor.m
@@ -12,10 +12,10 @@ function wval = MultiStageCompressor(pi)
 MStage = length(pi)+1;
 NState = length(pi)+2;
 %
-syms p v T
+% syms p v T
 % 假定工质为理想气体
 Rg = 0.287e3; n = 1.3; 
-eos = p*v == Rg*T;
+% eos = p*v == Rg*T;
 % 
 wval = zeros(MStage,1);
 pval = zeros(NState,1);
@@ -31,26 +31,30 @@ pval(NState) = 12.5e6;
 
 %% 顺次表达状态点性质   
 % 状态1性质（工质初态）
-vval(1) = eval(subs(solve(eos, v), [p T], [pval(1) Tval(1)]));
+% vval(1) = eval(subs(solve(eos, v), [p T], [pval(1) Tval(1)]));
+vval(1) = Rg*Tval(1)/pval(1);
 for i = 1:MStage
     % 压缩前状态
     p1 = pval(i); v1 = vval(i); T1 = Tval(i);
     % 压缩后状态
     p2 = pval(i+1);
     % 工质从状态1通过多变过程（n=1.3）变化到状态2
-    syms dlnp dlnv
-    eq1 = dlnp+n*dlnv == 0;
-    eq1a = subs(eq1, [dlnp,dlnv], [log(p)-log(p1),log(v)-log(v1)]);
-    v2 = subs(solve(eq1a, v), p, p2);
-    T2 = eval(subs(solve(eos, T), [p v], [p2 v2]));
-    % 压缩过程功耗
-    wval(i) = -eval(int(solve(eq1a, v), p, p1, p2));
-    % 级间等压冷却
-    v2 = subs(solve(eos, v), [p T], [p2 T2]);
-    % 写入状态变量
-    pval(i+1) = p2;
-    vval(i+1) = v2;
+%     syms dlnp dlnv
+%     eq1 = dlnp+n*dlnv == 0;
+%     eq1a = subs(eq1, [dlnp,dlnv], [log(p)-log(p1),log(v)-log(v1)]);
+%     v2 = subs(solve(eq1a, v), p, p2);
+    v2 = v1*exp((log(p1) - log(p2))/n);
+%     T2 = eval(subs(solve(eos, T), [p v], [p2 v2]));
+    T2 = p2*v2/Rg;
     Tval(i+1) = T2;
+    % 压缩过程功耗
+%     wval(i) = -eval(int(solve(eq1a, v), p, p1, p2));
+    w = @(p)((p1./p).^(1/n)*v1);
+    wval(i) = integral(w, p1, p2);    
+    % 级间等压冷却
+%     v2 = subs(solve(eos, v), [p T], [p2 T2]);
+    v2 = Rg*Tval(1)/p2;
+    vval(i+1) = v2;    
 end
 
 end
