58d689eb33b8f180a960e7d1c22b8b20662cb9a0
GGQ_sb1703 <gqguan@hotmail.com>
2020-04-09 13:25:22 +0800

    [feature] 添加工质物性和过程特征从主程序调用的功能

--- MultiStageCompressor.m
+++ MultiStageCompressor.m
@@ -1,12 +1,14 @@
 %% 多级压缩总功耗计算函数
 %
 % 调用参数说明
-% pi - (i, double array) 各级压缩后的工质压力向量
-% w  - (o, double array) 各级压缩的功耗向量
+% pi       - (i, double array) 各级压缩后的工质压力向量
+% material - (i, struct array(2)) 初、终态工质性质
+% process  - (i, struct array(NStage)) 各级压缩过程特性
+% wval     - (o, double array) 各级压缩的功耗向量
 %
 % by Dr. Guan Guoqiang @ SCUT on 2020-04-09
 %
-function wval = MultiStageCompressor(pi)
+function [wval,pval,vval,Tval] = MultiStageCompressor(pi, materials, processes)
 %% 初始化
 % 通过pi分量个数得压缩级数
 MStage = length(pi)+1;
@@ -14,7 +16,7 @@ NState = length(pi)+2;
 %
 % syms p v T
 % 假定工质为理想气体
-Rg = 0.287e3; n = 1.3; 
+Rg = materials(1).Rg; 
 % eos = p*v == Rg*T;
 % 
 wval = zeros(MStage,1);
@@ -23,11 +25,11 @@ vval = zeros(NState,1);
 Tval = zeros(NState,1);
 % 代入已知条件
 % 初态
-pval(1) = 0.1e6; Tval(1) = 20+273.15; % 采用国际单位（下同）
+pval(1) = materials(1).p; Tval(1) = materials(1).T; % 采用国际单位（下同）
 % 中间态
 pval(2:NState-1) = pi;
 % 终态
-pval(NState) = 12.5e6;
+pval(NState) = materials(2).p;
 
 %% 顺次表达状态点性质   
 % 状态1性质（工质初态）
@@ -39,6 +41,7 @@ for i = 1:MStage
     % 压缩后状态
     p2 = pval(i+1);
     % 工质从状态1通过多变过程（n=1.3）变化到状态2
+    n = processes(i).n;
 %     syms dlnp dlnv
 %     eq1 = dlnp+n*dlnv == 0;
 %     eq1a = subs(eq1, [dlnp,dlnv], [log(p)-log(p1),log(v)-log(v1)]);
