34f5e865b17ab88492a69025a3ca0bc94ff53abf
GGQ_sb1703 <gqguan@hotmail.com>
2020-04-09 13:37:02 +0800

    [feature] 增加计算不同级压缩的功耗

--- c8e2.m
+++ c8e2.m
@@ -4,51 +4,36 @@
 %
 %% 初始化
 clear;
-syms p v T
-% 假定工质为理想气体
-Rg = 0.287e3; n = 1.3; 
-eos = p*v == Rg*T;
+% 定义数据结构
+material = struct('Name', "Air", 'p', 0.1e6, 'v', [], 'T', 20+273.15, 'm', 120/3600, 'Rg', 287);
+process = struct('n', 1.3);
 
-%% 顺次表达状态点性质
-% 状态1性质（工质初态）
-p1 = 0.1e6; T1 = 20+273.15;
-v1 = eval(subs(solve(eos, v), [p T], [p1 T1]));
-% 状态2性质（1级压缩后）
-syms p2
-% 工质从状态1通过多变过程（n=1.3）变化到状态2
-syms dlnp dlnv
-eq1 = dlnp+n*dlnv == 0;
-eq1a = subs(eq1, [dlnp,dlnv], [log(p)-log(p1),log(v)-log(v1)]);
-v2 = subs(solve(eq1a, v), p, p2);
-T2 = eval(subs(solve(eos, T), [p v], [p2 v2]));
-% 压缩过程功耗
-w1 = -int(solve(eq1a, v), p, p1, p2);
-% 级间等压冷却
-v2 = subs(solve(eos, v), [p T], [p2 T2]);
-% 状态3性质（2级压缩后）
-syms p3
-eq1b = subs(eq1, [dlnp,dlnv], [log(p)-log(p2),log(v)-log(v2)]);
-v3 = subs(solve(eq1b, v), p, p3);
-T3 = eval(subs(solve(eos, T), [p v], [p3 v3]));
-% 压缩过程功耗
-w2 = -int(solve(eq1b, v), p, p2, p3);
-% 级间等压冷却
-v3 = subs(solve(eos, v), [p T], [p3 T2]);
-% 状态4性质（3级压缩后，工质终态)
-p4 = 12.5e6;
-eq1c = subs(eq1, [dlnp,dlnv], [log(p)-log(p3),log(v)-log(v3)]);
-v4 = subs(solve(eq1c, v), p, p4);
-T4 = eval(subs(solve(eos, T), [p v], [p4 v4]));
-% 压缩过程功耗
-w3 = -int(solve(eq1c, v), p, p3, p4);
-%% 总功耗
-w = w1+w2+w3;
+%%
+% 命令行输入计算条件
+fprintf('Calculating the work for ideal gas (air) in a multi-stage compressor ...\n')
+NStage = input('Please input the number of stage: ');
+% 初值
+materials(1:2) = material;
+processes(1:NStage) = process;
+materials(2).p = 12.5e6;
 
 %% 求最小功耗
 % 目标函数
-work = @(pval)(sum(abs(MultiStageCompressor(pval))));
+work = @(pval)(sum(abs(MultiStageCompressor(pval, materials, processes))));
 % 初值
-pval0 = [(p4-p1)/20,(p4-p1)/5];
+pval0 = zeros(NStage-1,1);
+for i = 1:(NStage-1)
+    pval0(i) = (materials(2).p-materials(1).p)/(NStage-i+5);
+end
 % 求使目标函数最小的pval值
 options = optimset('PlotFcns', @optimplotfval);
-[pi,w_min,exitflag] = fminsearch(work, pval0, options)
+[pi,w_min,exitflag] = fminsearch(work, pval0, options);
+
+%% 输出结果
+% 列表输出各级压缩的功耗及出口工质状态
+StageNum = [1:NStage]';
+[Work,pout,vout,Tout] = MultiStageCompressor(pi, materials, processes);
+pout = pout(2:end);
+vout = vout(2:end);
+Tout = Tout(2:end);
+table(StageNum, Work, pout, vout, Tout)
