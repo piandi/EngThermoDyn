2fa1e408550be0e5be754f6b99de3650c2ac6998
GGQ_TP2101 <gqguan@hotmail.com>
2022-02-23 05:08:33 +0800

    修改结果输出时的小数点显示方式

--- c1a16.m
+++ c1a16.m
@@ -4,9 +4,9 @@ syms p V;
 V1 = 0.1; V2 = 0.25; pa = 0.1; friction = 1200; area = 0.1;
 p = @(V)(0.24-0.4*V);
 Wexp = int(p(V), V, V1, V2);
-fprintf('Ans.1: expansion work is %.3f MJ.\n', eval(Wexp));
+fprintf('Ans.1: expansion work is %.3g MJ.\n', eval(Wexp));
 Weff = int(p(V)-pa-friction/area/1e6, V, V1, V2);
-fprintf('Ans.2: effective work is %.3f MJ.\n', eval(Weff));
+fprintf('Ans.2: effective work is %.3g MJ.\n', eval(Weff));
 Wuti = int(p(V)-pa, V, V1, V2);
-fprintf('Ans.3: effective work with ignored friction is %.3f MJ.\n', ...
+fprintf('Ans.3: effective work with ignored friction is %.3g MJ.\n', ...
     eval(Wuti));
